d90007afd3151d3b95d73491e037af0fb7929382
Ornella Fifa <ornellafifa@gmail.com>
2021-11-30 12:00:58 +0100

    dernière modification de la page contact

--- vue/connect_vue/contactus.php
+++ vue/connect_vue/contactus.php
@@ -1,133 +1,135 @@
 <main>
-    <section class="py-5 text-center container">
-        <div class="row py-sm-5">
-            <div class="col-sm-6 col-md-8 mx-auto">
+    <!-- <section class="py-5 text-center container"> -->
+        <!-- <div class="row py-sm-5"> -->
+            <?php 
+            //if( isset( $_REQUEST['error'] ) && $_REQUEST['error'] == md5( 'envoyer' ) ){
+                ?>
+                <!-- <div class="alert alert-success" role="alert">
+                    Message envoyé avec succès
+                </div> -->
+                <?php
+            //} 
+            //if( isset( $_REQUEST['error'] ) && $_REQUEST['error'] == md5( 'NoSend' ) ){
+                ?>
+                <!-- <div class="alert alert-success" role="alert">
+                    Désolé votre message n'a pas été envoyer.
+                </div> -->
+                <?php
+            //} 
+            ?>
+            <!-- <div class="col-sm-6 col-md-8 mx-auto">
                 <h1 class="fw-light " style="text-shadow:1px 1px 1px black;">Contactez-Nous</h1>
-                <form class="needs-validation" novalidate>
+                <form class="needs-validation" method="POST" action="../module/connect_mod/contactus.php" novalidate>
                     <div class="form-row">
-                        
-                        <div class="col-md-12 mb-3 ">
-                            <br><br>
+                        <div class="col-sm-12 mb-3 pt-4">
                             <div class="input-group">
                                 <div class="input-group-prepend">
                                     <span class="input-group-text" id="inputGroupPrepend">@</span>
                                 </div>
-                                <input type="text" class="form-control" id="validationCustomUsername" placeholder="Email" aria-describedby="inputGroupPrepend" required>
-                                <div class="invalid-feedback">
-                                    Please choose a username.
-                                </div>
+                                <input type="text" name="email" class="form-control" placeholder="Email" required>
                             </div>
-                            <div class="form-group">
-                            <br><br>
-                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Message"></textarea>
+                            <div class="form-group pt-4">
+                                <textarea class="form-control" name="messages" id="exampleFormControlTextarea1" rows="4" placeholder="Messages ..."></textarea>
                             </div>
                         </div>
-         
                     </div>
-            </div>
-          
-        </div>
-       
-        <button class="btn btn-success" type="submit">Submit form</button>
-        </form>
-        </div>
-        </div>
-    </section>
+                    <button class="btn btn-success" type="submit">Submit form</button>
+                </form>
+            </div> -->
+        <!-- </div> -->
+    <!-- </section> -->
 
-      <div class="row py-5" style="margin-left: 30px; margin-right:30px; ">
-
-            <div class="card col-md-3" style="width:18rem;margin-bottom:15px;">
-                <img  class="card-img-top" src="../image/aut.jpg" alt="Card image cap" style="width:286px; height:180px;">
-                <div class="card-body">
-                    <h5 class="card-title" style="font-weight: bold; font-style:italic; font-family:fantasy; text-shadow:1px 1px 1px black;">GBOYOU Charles</h5>
-                    <p class="card-text"> PHP JAVA C SQL SYMFONY</p>
-                    <p class="card-text"> Chef Projet & Responsable Back-End </p>
-                    <a href="#" class="btn btn-primary">Demander CV</a>
-                </div>
-            </div>
-
-            <div class="card col-md-3" style="width:18rem; margin-bottom:15px;" >
-            <img  class="card-img-top" src="../image/orne.jpg" alt="Card image cap" style="width:280px; height:180px; " >
+    <div class="row py-5" style="margin-left: 30px; margin-right:30px; ">
+        <div class="card col-md-3" style="width:18rem;margin-bottom:15px;">
+            <img class="card-img-top" src="../image/aut.jpg" alt="Card image cap" style="width:286px; height:180px;">
             <div class="card-body">
-                <h5 class="card-title" style="font-weight: bold; font-style:italic; font-family:fantasy; text-shadow:1px 1px 1px black;" >AHOUANDOGBO Mirlande</h5>
-                <p class="card-text">  PHP SQL JAVA BOOTSTRAP</p>
-                <p class="card-text">  Front-End Rédaction Web </p>
+                <h5 class="card-title" style="font-weight: bold; font-style:italic; font-family:fantasy;">GBOYOU Charles</h5>
+                <p class="card-text"> PHP JAVA C SQL SYMFONY</p>
+                <p class="card-text"> Chef Projet & Responsable Back-End </p>
+                <a href="#" class="btn btn-primary">Demander CV</a>
+            </div>
+        </div>
+
+        <div class="card col-md-3" style="width:18rem; margin-bottom:15px;">
+            <img class="card-img-top" src="../image/orne.jpg" alt="Card image cap" style="width:280px; height:180px; ">
+            <div class="card-body">
+                <h5 class="card-title" style="font-weight: bold; font-style:italic; font-family:fantasy;">AHOUANDOGBO Mirlande</h5>
+                <p class="card-text"> PHP SQL JAVA BOOTSTRAP</p>
+                <p class="card-text"> Front-End Rédaction Web </p>
                 <a href="#" class="btn btn-primary">Demander CV </a>
             </div>
-            </div>
+        </div>
 
-            <div class="card col-md-3" style="width:18rem; margin-bottom:15px;">
-            <img  class="card-img-top" src="../image/aut.jpg" alt="Card image cap" style="width:286px; height:180px; ">
+        <div class="card col-md-3" style="width:18rem; margin-bottom:15px;">
+            <img class="card-img-top" src="../image/aut.jpg" alt="Card image cap" style="width:286px; height:180px; ">
             <div class="card-body">
-                <h5 class="card-title" style="font-weight: bold; font-style:italic; font-family:fantasy; text-shadow:1px 1px 1px black;">AMOUSSOU Auguste-Marie</h5>
-                <p class="card-text">  PHP SQL</p>
+                <h5 class="card-title" style="font-weight: bold; font-style:italic; font-family:fantasy;">AMOUSSOU Auguste-Marie</h5>
+                <p class="card-text"> PHP SQL</p>
                 <p class="card-text"> Back-End Connexion </p>
                 <a href="#" class="btn btn-primary">Demander CV</a>
             </div>
-            </div>
+        </div>
 
-            
-            <div class="card col-md-3" style="width:18rem;margin-bottom:15px;">
-            <img  class="card-img-top" src="../image/aut.jpg" alt="Card image cap" style="width:286px; height:180px;">
+
+        <div class="card col-md-3" style="width:18rem;margin-bottom:15px;">
+            <img class="card-img-top" src="../image/aut.jpg" alt="Card image cap" style="width:286px; height:180px;">
             <div class="card-body">
-                <h5 class="card-title" style="font-weight: bold; font-style:italic; font-family:fantasy; text-shadow:1px 1px 1px black;">DJOSSA Bernice</h5>
-                <p class="card-text">  PHP SQL</p>
+                <h5 class="card-title" style="font-weight: bold; font-style:italic; font-family:fantasy;">DJOSSA Bernice</h5>
+                <p class="card-text"> PHP SQL</p>
                 <p class="card-text"> Back-End Inscription</p>
                 <a href="#" class="btn btn-primary">Demander CV</a>
             </div>
-            </div>
-       
+        </div>
 
 
-
-            <div class="card col-md-3" style="width:18rem; margin-bottom:15px;" >
-            <img  class="card-img-top" src="../image/aut.jpg" alt="Card image cap" style="width:280px; height:180px;" >
+        <div class="card col-md-3" style="width:18rem; margin-bottom:15px;">
+            <img class="card-img-top" src="../image/aut.jpg" alt="Card image cap" style="width:280px; height:180px;">
             <div class="card-body">
-                <h5 class="card-title" style="font-weight: bold; font-style:italic; font-family:fantasy; text-shadow:1px 1px 1px black;">ADISSA Nimath</h5>
-                <p class="card-text">  PHP C SQL</p>
+                <h5 class="card-title" style="font-weight: bold; font-style:italic; font-family:fantasy;">ADISSA Nimath</h5>
+                <p class="card-text"> PHP C SQL</p>
                 <p class="card-text"> Formulaire d'ajout ou de modification d'un membre </p>
                 <a href="#" class="btn btn-primary">Demander CV</a>
             </div>
-            </div>
+        </div>
 
-           
 
-           
-            <div class="card col-md-3" style="width:18rem;margin-bottom:15px;">
-            <img  class="card-img-top" src="../image/aut.jpg" alt="Card image cap" style="width:286px; height:180px;">
+
+
+        <div class="card col-md-3" style="width:18rem;margin-bottom:15px;">
+            <img class="card-img-top" src="../image/aut.jpg" alt="Card image cap" style="width:286px; height:180px;">
             <div class="card-body">
-                <h5 class="card-title" style="font-weight: bold; font-style:italic; font-family:fantasy; text-shadow:1px 1px 1px black;">AMOUSSOU Bérida</h5>
-                <p class="card-text">  C++ JAVA</p>
+                <h5 class="card-title" style="font-weight: bold; font-style:italic; font-family:fantasy;">AMOUSSOU Bérida</h5>
+                <p class="card-text"> C++ JAVA</p>
                 <p class="card-text">Détails sur un membre </p>
                 <a href="#" class="btn btn-primary">Demander CV</a>
             </div>
-            </div>
-            
-
-           
-
-          
-            <div class="card col-md-3" style="width:18rem;margin-bottom:15px;">
-            <img  class="card-img-top" src="../image/aut.jpg" alt="Card image cap" style="width:286px; height:180px;">
-            <div class="card-body">
-                <h5 class="card-title" style="font-weight: bold; font-style:italic; font-family:fantasy; text-shadow:1px 1px 1px black;">EDJEKPOTO Auréole</h5>
-                <p class="card-text">  SQL C PHP</p>
-                <p class="card-text"> Insertion Back-End </p>
-                <a href="#" class="btn btn-primary">Voir CV</a>
-            </div>
-            </div>
-            
-            
-
-            <div class="card col-md-3" style="width:18rem;margin-bottom:15px;">
-            <img  class="card-img-top" src="../image/aut.jpg" alt="Card image cap" style="width:286px; height:180px;">
-            <div class="card-body">
-                <h5 class="card-title" style="font-weight: bold; font-style:italic; font-family:fantasy; text-shadow:1px 1px 1px black;">KPANNOU Steven</h5>
-                <p class="card-text"> CSS PHP</p>
-                <p class="card-text">  Liste des questions</p>
-                <a href="#" class="btn btn-primary">Voir CV</a>
-            </div>
-            </div>
-           
         </div>
+
+
+
+
+
+        <div class="card col-md-3" style="width:18rem;margin-bottom:15px;">
+            <img class="card-img-top" src="../image/aut.jpg" alt="Card image cap" style="width:286px; height:180px;">
+            <div class="card-body">
+                <h5 class="card-title" style="font-weight: bold; font-style:italic; font-family:fantasy;">EDJEKPOTO Auréole</h5>
+                <p class="card-text"> SQL C PHP</p>
+                <p class="card-text"> Insertion Back-End </p>
+                <a href="#" class="btn btn-primary">Demander CV</a>
+            </div>
+        </div>
+
+
+
+        <div class="card col-md-3" style="width:18rem;margin-bottom:15px;">
+            <img class="card-img-top" src="../image/aut.jpg" alt="Card image cap" style="width:286px; height:180px;">
+            <div class="card-body">
+                <h5 class="card-title" style="font-weight: bold; font-style:italic; font-family:fantasy;">KPANNOU Steven</h5>
+                <p class="card-text"> CSS PHP</p>
+                <p class="card-text"> Liste des questions</p>
+            </div>
+        </div>
+    </div>
 </main>
+
+
